aa5e9abbcf156bacbc6afd7520b7869892b523c3
p3tr0ne12 <brandonpetrone9@gmail.com>
2024-10-07 13:29:35 -0700

    movement tweak

--- Unity_Intro_PM/Assets/Scripts/Pausemenu.cs
+++ Unity_Intro_PM/Assets/Scripts/Pausemenu.cs
@@ -5,19 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Pausemenu : MonoBehaviour
 {
-    public string mainScene;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
+    
     public void Quitgame()
     {
         SceneManager.LoadScene(mainScene);
--- Unity_Intro_PM/Assets/Scripts/mainmenu.cs
+++ Unity_Intro_PM/Assets/Scripts/mainmenu.cs
@@ -7,17 +7,6 @@ public class mainmenu : MonoBehaviour
 {
 
     public string level1Scene;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void Playgame()
     {
